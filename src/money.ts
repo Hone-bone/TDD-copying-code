2d717e4f5dbcf09d7b203b7d36924ad7ae6bb720
Susanock <allmasterleo@gmail.com>
2024-04-11 00:34:15 +0900

    第８章
    Moneyクラスを抽象クラスに一旦書き換えたため、テストが通らないことに気づかず、時間がかかった。

--- src/money.ts
+++ src/money.ts
@@ -1,10 +1,23 @@
-export class Money {
+import { Dollar } from "./dollar";
+import { Franc } from "./franc";
+
+export abstract class Money {
   protected amount: number;
+
+  abstract times(multiplier: number): Money;
 
   equals(object: Object): boolean {
     const money = object as Money;
     return (
       this.amount === money.amount && this.constructor === money.constructor
     );
+  }
+
+  static dollar(amount: number): Money {
+    return new Dollar(amount);
+  }
+
+  static franc(amount: number): Money {
+    return new Franc(amount);
   }
 }
